dff358a191324f4c9f02f18653b455aae0b664ef
João Villela <joao.pcv@puccampinas.edu.br>
2026-04-06 11:02:40 -0300

    Create main.py with election system description
    Add main file with election system functionalities overview

---
+++ main.py
@@ -0,0 +1,3 @@
+# Arquivo principal do sistema eleitoral.
+# Responsável por executar o menu e controlar as funcionalidades:
+# cadastro de candidatos, cadastro de eleitores, votação e apuração dos votos.
